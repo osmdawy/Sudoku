bbe0d0b3026ce40397af3a1354c41748ea2dfce6
Omnia Saad ElDawy <osmdawy@gmail.com>
2013-05-07 23:24:11 +0300

    Bluetooth -- updated

--- src/com/example/sudoku/Game.java
+++ src/com/example/sudoku/Game.java
@@ -2,15 +2,27 @@ package com.example.sudoku;
 
 
 
+
+
+
+
+
+
 import android.app.Activity;
 import android.app.Dialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 public class Game extends Activity {
@@ -49,8 +61,18 @@ public class Game extends Activity {
     private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
     private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
     private static final int REQUEST_ENABLE_BT = 3;
+    
+    
+    // Local Bluetooth adapter
+    private BluetoothAdapter mBluetoothAdapter = null;
+    // Member object for the chat services
+    private BluetoothChatService mChatService = null;
 	// private SharedPreferences preferences;
 
+    // Name of the connected device
+    private String mConnectedDeviceName = null;
+    // String buffer for outgoing messages
+    private StringBuffer mOutStringBuffer;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 
@@ -114,9 +136,83 @@ public class Game extends Activity {
 
 		}
 		new_game = false;
+		  // Get local Bluetooth adapter
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // If the adapter is null, then Bluetooth is not supported
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
 
 	}
+	 public void onStart() {
+	        super.onStart();
+	      
 
+	        // If BT is not on, request that it be enabled.
+	        // setupChat() will then be called during onActivityResult
+	        if (!mBluetoothAdapter.isEnabled()) {
+	            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+	            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+	        // Otherwise, setup the chat session
+	        } else {
+	            if (mChatService == null){
+	            	 setupChat();
+	            }
+	        }
+	    }
+	 @Override
+	    public synchronized void onResume() {
+	        super.onResume();
+	       
+	        // Performing this check in onResume() covers the case in which BT was
+	        // not enabled during onStart(), so we were paused to enable it...
+	        // onResume() will be called when ACTION_REQUEST_ENABLE activity returns.
+	        if (mChatService != null) {
+	            // Only if the state is STATE_NONE, do we know that we haven't started already
+	            if (mChatService.getState() == BluetoothChatService.STATE_NONE) {
+	              // Start the Bluetooth chat services
+	              mChatService.start();
+	            }
+	        }
+	    }
+	 public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//	        if(D) Log.d(TAG, "onActivityResult " + resultCode);
+	        switch (requestCode) {
+	        case REQUEST_CONNECT_DEVICE_SECURE:
+	            // When DeviceListActivity returns with a device to connect
+	            if (resultCode == Activity.RESULT_OK) {
+	                connectDevice(data, true);
+	            }
+	            break;
+	        case REQUEST_CONNECT_DEVICE_INSECURE:
+	            // When DeviceListActivity returns with a device to connect
+	            if (resultCode == Activity.RESULT_OK) {
+	                connectDevice(data, false);
+	            }
+	            break;
+	        case REQUEST_ENABLE_BT:
+	            // When the request to enable Bluetooth returns
+	            if (resultCode == Activity.RESULT_OK) {
+	                // Bluetooth is now enabled, so set up a chat session
+	                setupChat();
+	            } else {
+	                // User did not enable Bluetooth or an error occured
+	                Log.d(TAG, "BT not enabled");
+//	                Toast.makeText(this, R.string.bt_not_enabled_leaving, Toast.LENGTH_SHORT).show();
+	                finish();
+	            }
+	        }
+	    }
+	 private void setupChat(){
+		 // Initialize the BluetoothChatService to perform bluetooth connections
+         mChatService = new BluetoothChatService(this, mHandler);
+
+         // Initialize the buffer for outgoing messages
+         mOutStringBuffer = new StringBuffer("");
+	 }
 	protected void showKeypadOrError(int x, int y) {
 		// check if empty cell
 		if (puzzle[y * 9 + x] == 0) {
@@ -238,5 +334,62 @@ public class Game extends Activity {
 	        }
 	        return false;
 	    }
+	    
+	    private void connectDevice(Intent data, boolean secure) {
+	        // Get the device MAC address
+	        String address = data.getExtras()
+	            .getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+	        // Get the BLuetoothDevice object
+	        BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+	        // Attempt to connect to the device
+	        mChatService.connect(device, secure);
+	    }
+	 // The Handler that gets information back from the BluetoothChatService
+	    private final Handler mHandler = new Handler() {
+	        @Override
+	        public void handleMessage(Message msg) {
+	            switch (msg.what) {
+	            case MESSAGE_STATE_CHANGE:
+	               
+	                switch (msg.arg1) {
+	                case BluetoothChatService.STATE_CONNECTED:
+//	                    mTitle.setText(R.string.title_connected_to);
+//	                    mTitle.append(mConnectedDeviceName);
+//	                    mConversationArrayAdapter.clear();
+	                    break;
+	                case BluetoothChatService.STATE_CONNECTING:
+//	                    mTitle.setText(R.string.title_connecting);
+	                    break;
+	                case BluetoothChatService.STATE_LISTEN:
+	                case BluetoothChatService.STATE_NONE:
+//	                    mTitle.setText(R.string.title_not_connected);
+	                    break;
+	                }
+	                break;
+	            case MESSAGE_WRITE:
+	                byte[] writeBuf = (byte[]) msg.obj;
+	                // construct a string from the buffer
+	                String writeMessage = new String(writeBuf);
+//	                mConversationArrayAdapter.add("Me:  " + writeMessage);
+	                break;
+	            case MESSAGE_READ:
+	                byte[] readBuf = (byte[]) msg.obj;
+	                // construct a string from the valid bytes in the buffer
+	                String readMessage = new String(readBuf, 0, msg.arg1);
+//	                mConversationArrayAdapter.add(mConnectedDeviceName+":  " + readMessage);
+	                break;
+	            case MESSAGE_DEVICE_NAME:
+	                // save the connected device's name
+	                mConnectedDeviceName = msg.getData().getString(DEVICE_NAME);
+	                Toast.makeText(getApplicationContext(), "Connected to "
+	                               + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
+	                break;
+	            case MESSAGE_TOAST:
+	                Toast.makeText(getApplicationContext(), msg.getData().getString(TOAST),
+	                               Toast.LENGTH_SHORT).show();
+	                break;
+	            }
+	        }
+	    };
 
 }
